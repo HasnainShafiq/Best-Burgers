8eecb753a3649106c3d4c710c03b0f2e64c8c796
Uncreative-creativity <31780746+Uncreative-creativity@users.noreply.github.com>
2021-12-17 19:34:41 +0000

    Add files via upload

---
+++ DamnIt/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/index.html", "./src/app.js"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-lg": "url('/src/Daco_5766928.png')",
+        "hero-sm": "url('/src/Burger-PNG-Image.png')"  
+      },
+      fontFamily: {
+        "Lato": ['Lato', 'sans-serif'],
+
+        "Open-Sans": ["'Open Sans'", 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
